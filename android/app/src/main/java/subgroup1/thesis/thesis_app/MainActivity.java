f9b844f9b6057d0477c83ab3ed69ac7eb6a35bc9
theaisabellelangit <2190275@slu.edu.ph>
2022-02-06 00:55:06 +0800

    Initial commit

---
+++ android/app/src/main/java/subgroup1/thesis/thesis_app/MainActivity.java
@@ -0,0 +1,6 @@
+package subgroup1.thesis.thesis_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/java/thesis/subgroup1/thesis_app/MainActivity.java
@@ -0,0 +1,6 @@
+package thesis.subgroup1.thesis_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
